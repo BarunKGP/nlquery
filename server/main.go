d42a3d7a7b299168be4179dcc8c3e552e8a07ddc
Das, Barun <bdas31@gatech.edu>
2024-08-30 23:39:13 -0400

    added makefile

--- server/main.go
+++ server/main.go
@@ -8,19 +8,11 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-// logger := CreateLogger()
-
 func enableCors(w *http.ResponseWriter) {
 	// Replace this with env var?
 	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 
 }
-
-// func Index(w htt.ResponseWriter, r *http.Request, _ httprouter.Params) {
-//
-//   fmt.Fprintf(w, "Hello from nlQuery!\n")
-//   logger.Info("Home route")
-//   }
 
 func main() {
 	logger := CreateLogger()
@@ -31,6 +23,7 @@ func main() {
 		fmt.Fprintf(w, "Hello from nlQuery!\n")
 	})
 
+	logger.Info("Starting http server")
 	log.Fatal(http.ListenAndServe(":8001", router))
 
 }
