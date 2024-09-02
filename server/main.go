6ebd7a128caa50220b39082d69e78bcfd70bde3b
Das, Barun <bdas31@gatech.edu>
2024-09-02 13:14:27 -0400

    update main.go

--- server/main.go
+++ server/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -14,16 +16,57 @@ func enableCors(w *http.ResponseWriter) {
 
 }
 
+type Signin struct {
+	User any `json:user`
+	Account string `json:account`
+}
+
+type User struct {
+	Id string `json: id`
+	Name string `json: name`
+	Email string `json: email`
+	ImageSrc string `json: image`
+}
+
 func main() {
 	logger := CreateLogger()
 	router := httprouter.New()
+	
 	router.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 		logger.Info("Home route")
 		fmt.Fprintf(w, "Hello from nlQuery!\n")
 	})
 
+	router.POST("/api/v1/auth/signin", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		body, err := io.ReadAll(r.Body); if err != nil {
+			errMsg := "Unable to read body"
+			logger.Error(errMsg, err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Header().Add("X-Error", errMsg)
+
+			return
+		}
+
+		var signinBody Signin
+		err = json.Unmarshal(body, &signinBody); if err != nil {
+				errMsg := "Unable to decode signin body"
+				logger.Error(errMsg, err)
+				w.WriteHeader(http.StatusBadRequest)
+				w.Header().Add("X-Error", errMsg)
+
+				return
+		}
+
+
+		// logger.Info("On signin route")
+		// logger.Debug(str(signinBody))
+	})
+
+
+
 	logger.Info("Starting http server")
 	log.Fatal(http.ListenAndServe(":8001", router))
 
 }
+
