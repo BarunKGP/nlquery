926226b760a55cbf179ae305f134a9d4ae4a4403
Das, Barun <bdas31@gatech.edu>
2024-10-03 01:09:35 -0400

    update backend session establishment

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -24,62 +24,70 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         console.log("Found user");
         token.id = user.id;
 
-        let response, data;
-        try {
-          response = await fetch(`${process.env.BACKEND_SERVER}/auth/signin`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              id: user.id,
-              email: user.email,
-              name: user.name,
-              image: user.image,
-            }),
-            credentials: "include",
-          });
-        } catch (err) {
-          console.log("Could not fetch data from backend: " + err);
-        }
-        try {
-          data = await response.json();
-        } catch (err) {
-          console.log("Could not deserialize json: " + err + ` ${response}`);
-        }
-
-        token.backendToken = data.token;
+        // let response, data;
+        // try {
+        //   response = await fetch(`${process.env.BACKEND_SERVER}/auth/signin`, {
+        //     method: "POST",
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify({
+        //       id: user.id,
+        //       email: user.email,
+        //       name: user.name,
+        //       image: user.image,
+        //     }),
+        //     credentials: "include",
+        //   });
+        // } catch (err) {
+        //   console.log("Could not fetch data from backend: " + err);
+        // }
+        // try {
+        //   data = await response.json();
+        // } catch (err) {
+        //   console.log("Could not deserialize json: " + err + ` ${response}`);
+        // }
       }
       return token;
     },
 
     async session({ session, token }) {
       session.user.id = token.id;
-      // let response, data;
-      // try {
-      //   response = await fetch(`${process.env.BACKEND_SERVER}/auth/signin`, {
-      //     method: "POST",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //     },
-      //     body: JSON.stringify({
-      //       id: session.user.id,
-      //       email: session.user.email,
-      //       name: session.user.name,
-      //       image: session.user.image,
-      //     }),
-      //     credentials: "include",
-      //   });
-      // } catch (err) {
-      //   console.log("Could not fetch data from backend: " + err);
-      // }
+      let response, data;
+      try {
+        response = await fetch(`${process.env.BACKEND_SERVER}/auth/signin`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: session.user.id,
+            email: session.user.email,
+            name: session.user.name,
+            image: session.user.image,
+          }),
+          credentials: "include",
+        });
+      } catch (err) {
+        console.log("Could not fetch data from backend: " + err);
+      }
       // try {
       //   data = await response.json();
       // } catch (err) {
       //   console.log("Could not deserialize json: " + err + ` ${response}`);
       // }
 
-      session.user.backendToken = data.token;
+      // if (response.ok) {
+      console.log("Success: session registered with backend");
+      //     return true;
+      //   } else {
+      //     // If there was an error storing the user details, you might want to prevent sign in
+      //     console.error(
+      //       `Error establishing backend session! Received status ${response.status}`
+      //     );
+      //     return false;
+
+      // session.user.backendToken = data.token;
       return session;
     },
 
@@ -103,17 +111,21 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     //   } catch (err) {
     //     console.log("Could not fetch data from backend: " + err);
     //   }
-    //   try {
-    //     data = await response.json();
-    //   } catch (err) {
-    //     console.log("Could not deserialize json: " + err + ` ${response}`);
-    //   }
+    //   // try {
+    //   //   data = await response.json();
+    //   // } catch (err) {
+    //   //   console.log("Could not deserialize json: " + err + ` ${response}`);
+    //   // }
 
     //   // If the API call was successful, allow sign in
     //   if (response.ok) {
+    //     console.log("Success: session registered with backend");
     //     return true;
     //   } else {
     //     // If there was an error storing the user details, you might want to prevent sign in
+    //     console.error(
+    //       `Error establishing backend session! Received status ${response.status}`
+    //     );
     //     return false;
     //   }
     // },
