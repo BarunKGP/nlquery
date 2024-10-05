5335d1ed55039789716525b021dcd4c8cea68408
Barun Das <barun.das.23@gmail.com>
2024-10-05 00:40:40 -0400

    add cards sql

---
+++ server/sql/query/cards.sql
@@ -0,0 +1,22 @@
+-- name: GetCard :one
+SELECT * FROM cards
+WHERE id = $1
+LIMIT 1;
+
+-- name: CreateCard :one
+INSERT INTO cards(query, userId) 
+VALUES ($1, $2)
+RETURNING *;
+
+-- name: GetUserCards: many
+SELECT (id, query, status, createdAt, lastModified)
+FROM cards  
+WHERE userId = $1;
+
+-- name: UpdateCardStatus :exec
+UPDATE cards
+SET status = $2, lastModified = (timezone('utc', now()))
+WHERE id = $1;
+
+
+
