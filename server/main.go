d6dcb94eff88fb96b0e15d25aed06d5c61ddc48c
Barun Das <barun.das.23@gmail.com>
2024-10-25 23:35:15 -0400

    refactor users

--- server/main.go
+++ server/main.go
@@ -5,13 +5,11 @@ import (
 
 	"github.com/BarunKGP/nlquery/adapters"
 	"github.com/BarunKGP/nlquery/adapters/controllers"
-	// "github.com/BarunKGP/nlquery/ports"
 )
 
 func main() {
 	gateway := adapters.NewGateway("/api/v1", []string{"http://localhost:3000"})
 	routes := controllers.InitRoutes()
-	// var _ ports.Routeable = routes
 	if err := gateway.Init(routes); err != nil {
 		log.Fatalf("Fatal! Initialization failed: %v", err)
 	}
