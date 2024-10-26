de40f73d4e3707bb5436e42cb4015d93f26a0872
Barun Das <barun.das.23@gmail.com>
2024-10-25 23:23:15 -0400

    refactor modules

--- server/ports/api.go
+++ server/ports/api.go
@@ -2,6 +2,11 @@ package ports
 
 import "net/http"
 
+type ApiObject interface {
+	ToJson() (string, error)
+	FromJson(string) (ApiObject, error)
+}
+
 type ApiErrorable interface {
 	Error() string
 	GetStatus() int
